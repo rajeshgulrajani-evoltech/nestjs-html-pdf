cb59e870b59b111c0cb0f521137b8731d8722141
lashakaraulashvili <lasha.karaulashvili94@gmail.com>
2023-08-31 15:06:24 +0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,40 @@
 const puppeteer = require('puppeteer');
-const hbs = require('handlebars')
-const fs = require('fs-extra')
+const hbs = require('handlebars');
+const fs = require('fs-extra');
 
 export const createPdf = async (filePath: string, options = {}, data = {}) => {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: 'new',
+    });
     const page = await browser.newPage();
-    
-    hbs.registerHelper("ifCond", function (
-        v1: any, 
-        operator: any, 
-        v2: any, 
-        options:any
-      ) {
+
+    hbs.registerHelper('ifCond', function(
+      v1: any,
+      operator: any,
+      v2: any,
+      options: any
+    ) {
       switch (operator) {
-        case "==":
+        case '==':
           return v1 == v2 ? options.fn(data) : options.inverse(data);
-        case "===":
+        case '===':
           return v1 === v2 ? options.fn(data) : options.inverse(data);
-        case "!=":
+        case '!=':
           return v1 != v2 ? options.fn(data) : options.inverse(data);
-        case "!==":
+        case '!==':
           return v1 !== v2 ? options.fn(data) : options.inverse(data);
-        case "<":
+        case '<':
           return v1 < v2 ? options.fn(data) : options.inverse(data);
-        case "<=":
+        case '<=':
           return v1 <= v2 ? options.fn(data) : options.inverse(data);
-        case ">":
+        case '>':
           return v1 > v2 ? options.fn(data) : options.inverse(data);
-        case ">=":
+        case '>=':
           return v1 >= v2 ? options.fn(data) : options.inverse(data);
-        case "&&":
+        case '&&':
           return v1 && v2 ? options.fn(data) : options.inverse(data);
-        case "||":
+        case '||':
           return v1 || v2 ? options.fn(data) : options.inverse(data);
         default:
           return options.inverse(options);
@@ -47,13 +49,13 @@ export const createPdf = async (filePath: string, options = {}, data = {}) => {
       lte: (v1: number, v2: number) => v1 <= v2,
       gte: (v1: number, v2: number) => v1 >= v2,
       and() {
-          return Array.prototype.every.call(arguments, Boolean);
+        return Array.prototype.every.call(arguments, Boolean);
       },
       or() {
-          return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
-      }
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+      },
     });
-    
+
     const html = await fs.readFile(filePath, 'utf8');
     const content = hbs.compile(html)(data);
     await page.setContent(content);
@@ -73,7 +75,7 @@ export const createPdf = async (filePath: string, options = {}, data = {}) => {
     await browser.close();
     // process.exit();
     return buffer;
-    } catch (e) {
+  } catch (e) {
     console.log(e);
-    }
+  }
 };
