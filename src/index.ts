beee44459f40d929629626b648da0da3142ae191
Lasha Karaulashvili <lasha.karaulashvili94@gmail.com>
2023-09-04 16:55:09 +0400

    test update

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,7 @@ const fs = require('fs-extra');
 export const createPdf = async (filePath: string, options = {}, data = {}) => {
   try {
     const browser = await puppeteer.launch({
-      headless: 'new',
       executablePath: process.env.CHROME_BIN || null,
-      args: [
-        '--no-sandbox',
-        '--headless',
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-      ],
     });
     const page = await browser.newPage();
 
